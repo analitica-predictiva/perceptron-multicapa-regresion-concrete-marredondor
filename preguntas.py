36e9232988abb8d1987cc14ee0755162bfab76fc
marredondor <marredondor@unal.edu.co>
2022-11-23 22:08:42 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -16,19 +16,19 @@ def pregunta_01():
     Carga y separación de los datos en `X` `y`
     """
     # Lea el archivo `concrete.csv` y asignelo al DataFrame `df`
-    df = ____  
+    df = pd.read_csv("concrete.csv", sep=",")
 
     # Asigne la columna `strength` a la variable `y`.
-    ____ = ____  
+    y = df['strength'].copy()  
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)  
+    X = df.copy()  
 
     # Remueva la columna `strength` del DataFrame `X`.
-    ____.____(____)  
+    X = df.drop('strength', axis=1)
 
     # Retorne `X` y `y`
-    return x, y
+    return X, y
 
 
 def pregunta_02():
